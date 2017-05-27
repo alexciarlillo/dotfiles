3f3725c5c7855a652d34ed23215e362b6b776850
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-05-27 18:32:18 -0400

    some visual tweaks

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,9 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # launch on each monitor
-xrandr --query | grep "[[:space:]]connected" | cut -d ' ' -f 1 | while read -r line ; do
-    MONITOR="$line" polybar top &
-    echo "Polybar started on $line"
+xrandr --query | grep "[[:space:]]connected" | while read -r line ; do
+    monitor=$( echo "$line" | cut -d' ' -f1 )
+    MONITOR="$monitor" polybar top &
+    echo "Polybar started on $line with width $width"
 done
 
