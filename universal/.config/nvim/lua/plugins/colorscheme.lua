cf6543cf76b6a33f8ef7627b4e5e91a0f82560be
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2023-07-17 11:27:56 -0400

    updates - mostly nvim

--- universal/.config/nvim/lua/plugins/colorscheme.lua
+++ universal/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,46 @@
 return {
-  -- add catppuccin
-  { "catppuccin/nvim" },
+ -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
 
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "catppuccin",
     },
   },
 }
--- universal/.config/nvim/lua/plugins/snippets.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "L3MON4D3/LuaSnip",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
-    end,
-  },
-}
