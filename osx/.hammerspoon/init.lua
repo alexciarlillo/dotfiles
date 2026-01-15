3cceb7c84bea5690b4a0a5a5ce8d4aa19d7d3ecd
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2026-01-15 12:13:54 -0500

    updates

--- osx/.hammerspoon/init.lua
+++ osx/.hammerspoon/init.lua
@@ -15,7 +15,7 @@ hs.hotkey.bind(hyper, "return", hs.hints.windowHints)
 
 -- Launch new wezterm window on current desktop
 hs.hotkey.bind({ "alt" }, "return", function()
-	hs.osascript.applescriptFromFile("/Users/alex/.bin/new-wezterm.applescript")
+	hs.osascript.applescriptFromFile("/Users/alex/.local/bin/new-wezterm.applescript")
 end)
 
 -- launch script on unlock and log to file
@@ -23,7 +23,7 @@ hs.caffeinate.watcher
 	.new(function(event)
 		if event == hs.caffeinate.watcher.screensDidUnlock then
 			-- log script with errors
-			hs.execute("/Users/alex/.bin/unlock > /tmp/unlock.log 2>&1", true)
+			hs.execute("/Users/alex/.local/bin/unlock > /tmp/unlock.log 2>&1", true)
 		end
 	end)
 	:start()
